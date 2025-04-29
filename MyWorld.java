04595bd81ef742e60d664fbe4e2c70ef2c10bfd6
Joe <53659792+joez-zh@users.noreply.github.com>
2025-04-29 14:13:24 -0400

    added random apple spawns

--- MyWorld.java
+++ MyWorld.java
@@ -13,7 +13,16 @@ public class MyWorld extends World {
         Elephant elephant = new Elephant();
         addObject(elephant, 300, 200);
         
+        createApple();
+    }
+    
+    /**
+     * Creates a new apple at a random location at the top of the screen
+     */
+    public void createApple() {
         Apple apple = new Apple();
-        addObject(apple, 300, 0);
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
     }
 }
