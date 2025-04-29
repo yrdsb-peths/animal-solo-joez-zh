2144e032dc48a612b3db15bd14bb806da45264b0
Joe <53659792+joez-zh@users.noreply.github.com>
2025-04-29 14:06:18 -0400

    added apple class

---
+++ Apple.java
@@ -0,0 +1,15 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Food for elephant
+ * 
+ * @author Joe
+ * @version May 2025
+ */
+public class Apple extends Actor
+{
+    public void act()
+    {
+        setLocation(getX(), getY() - 1);  
+    }
+}
