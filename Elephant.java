d815b4893e87363d215387434326c9f8fb03e4eb
Joe <53659792+joez-zh@users.noreply.github.com>
2025-04-29 14:35:17 -0400

    added background and changed speed of elephant

--- Elephant.java
+++ Elephant.java
@@ -8,13 +8,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Elephant extends Actor
 {
+    private int speed = 3;
     public void act()
     {
         if (Greenfoot.isKeyDown("left")) {
-            move(-1);
+            move(-speed);
         }
         else if (Greenfoot.isKeyDown("right")) {
-            move(1); 
+            move(speed); 
         }
         eat();
     }
