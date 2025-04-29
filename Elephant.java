4d1d8a2819415705b09a32d21847b34c34ca4657
Joe <53659792+joez-zh@users.noreply.github.com>
2025-04-29 14:15:17 -0400

    added apple respawn

--- Elephant.java
+++ Elephant.java
@@ -16,8 +16,18 @@ public class Elephant extends Actor
         else if (Greenfoot.isKeyDown("right")) {
             move(1); 
         }
-        
-        // removes apples when it touches the elephant
-        removeTouching(Apple.class);
+    }
+    
+    /**
+     * Eat the apple and spawns a new apple 
+     */
+    
+    public void eat() {
+        if (isTouching(Apple.class)) {
+            removeTouching(Apple.class);
+            
+            MyWorld world = (MyWorld) getWorld();
+            world.createApple();
+        }
     }
 }
