d2bb0221094318d812185b08689ee6d466941a9b
Joe <53659792+joez-zh@users.noreply.github.com>
2025-04-29 13:58:38 -0400

    placed elephant in the world

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,16 @@
 import greenfoot.*;
 
+/**
+ * The World our hero lives in.
+ * 
+ * @author Joe
+ * @version May 2025
+ */
+
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
     }
 }
