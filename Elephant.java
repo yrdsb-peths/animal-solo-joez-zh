b0770585b1d00d9270c3945ffa1ae50cbaa1d36f
Joe <53659792+joez-zh@users.noreply.github.com>
2025-04-29 13:56:33 -0400

    added Elephant

---
+++ Elephant.java
@@ -0,0 +1,15 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The Elephant, the Hero.
+ * 
+ * @author Joe
+ * @version May 2025
+ */
+public class Elephant extends Actor
+{
+    public void act()
+    {
+        move(1);
+    }
+}
