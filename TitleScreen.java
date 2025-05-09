77d1ba44519020053ff6f8612e3a83350f3eda25
Joe <53659792+joez-zh@users.noreply.github.com>
2025-05-09 19:00:01 -0400

    added an elephant image to the title screen

--- TitleScreen.java
+++ TitleScreen.java
@@ -14,19 +14,29 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        
+
         addObject(titleLabel, getWidth()/2, getHeight()/2);
+        prepare();
     }
-    
+
     /**
      * main world act loop
      */
-    
+
     public void act() {
         // starts the game when the user presses the space bar
         if (Greenfoot.isKeyDown("space")) {
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Elephant elephant = new Elephant();
+        addObject(elephant,293,114);
     }
 }
